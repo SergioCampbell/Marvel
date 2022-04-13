e9ca0780bfb370f572d8386398f38033e3ee8bac
Sergio Campbell Dev <51247757+SergioCampbell@users.noreply.github.com>
2022-04-12 23:45:42 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const PRIVATE_KEY = '70e28d8ebefea279f046dff41e1d1a1d7b5d631c'
-export const PUBLIC_KEY = 'c22fa726fa98df1bfcd6629ca6add564'
+export const PRIVATE_KEY = 'YOUR PRIVATE KEY'
+export const PUBLIC_KEY = 'YOUR PUBLIC KEY'
