11a389293b2c54e618f3714885f507bbaee04cd6
Sergio Campbell Dev <51247757+SergioCampbell@users.noreply.github.com>
2022-04-19 08:35:13 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import md5 from 'md5'
 
-export const PRIVATE_KEY = '70e28d8ebefea279f046dff41e1d1a1d7b5d631c'
-export const PUBLIC_KEY = 'c22fa726fa98df1bfcd6629ca6add564'
+export const PRIVATE_KEY = 'YOUR PRIVATE KEY'
+export const PUBLIC_KEY = 'YOUR PUBLIC KEY'
 export const ts = Date.now();
 export const cifrate = md5(ts + PRIVATE_KEY + PUBLIC_KEY);
